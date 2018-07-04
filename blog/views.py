a41ff96e640290a522bcea7caa01a9690052b25c
unknown <usernamer@example.com>
2018-07-04 12:24:14 +0800

    add templates

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+def post_list(request):
+	return render(request, 'blog/google_chart.html', {})
 
 # Create your views here.
